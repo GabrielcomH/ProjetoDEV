30e7a4fdbec354a62507de08adfd911c76b1c3d0
Ghabrielul <ghabriel.duarte@cs.cruzeirodosul.edu.br>
2025-05-13 14:22:49 -0300

    mod_tarefas,py_status_e_concluída

--- tarefas.py
+++ tarefas.py
@@ -15,7 +15,8 @@ def adicionar_tarefa(titulo, descricao, data, prioridade):
         "titulo": titulo,
         "descricao": descricao,
         "data": data,  
-        "prioridade": prioridade
+        "prioridade": prioridade,
+        "status": "pendente"  #status padrão
     }
     tarefas.append(nova_tarefa)
 
@@ -23,3 +24,19 @@ def adicionar_tarefa(titulo, descricao, data, prioridade):
         json.dump(tarefas, f, indent=4, ensure_ascii=False)
 
     print("✅ Tarefa adicionada com sucesso!") 
+
+def marcar_como_concluida(indice):
+    import json
+
+    CAMINHO_DO_ARQUIVO = "data/tarefa.json"
+    
+    with open(CAMINHO_DO_ARQUIVO, "w", encoding="utf=8") as f:
+        tarefa = json.load(f)
+
+    if 0 <= indice < len(tarefa):
+        tarefa[indice]["status"] = "concluída"
+        with open(CAMINHO_DO_ARQUIVO, "w", encoding="utf=8") as f:
+            json.dump(tarefa, f, indent=4, ensure_ascii=False)
+        print("\n✅ Tarefa marcada como concluída.\n")
+    else:
+        print("\n❌ Índice inválido.\n")
